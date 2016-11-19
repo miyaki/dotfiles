d21685c779b76909dda75000007a38540b6878f7
Takashi Miyaki <miyaki@acm.org>
2016-11-19 20:38:10 +0900

    add gosetup

---
+++ script/gosetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ `uname -s` == 'Darwin' ]; then
+  brew install go lv
+elif [ `uname -s` == 'Linux' ]; then
+  sudo apt-get install golang-go lv zsh
+fi
+
+unset $GOROOT
+export GOPATH=$HOME
+go get github.com/motemen/ghq
+go get github.com/peco/peco/cmd/peco
+go get github.com/github/hub
+ls ~/bin
+
